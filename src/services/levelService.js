58993ffccacd96da99589a1ba53095029692ea5f
Paul <pscheuber04@gmail.com>
2025-08-01 22:31:14 +0200

    Revert "chore: update file path in levelService.js to use relative path for levels.json"
    This reverts commit 60ddcf9e0d1307902645a3377fda1ca02f01ce37.

--- src/services/levelService.js
+++ src/services/levelService.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const filePath = 'levels.json';
+const filePath = path.join(__dirname, 'levels.json');
 module.exports = (client, arg) => {
     function getFile() {
         let pairs = [];
